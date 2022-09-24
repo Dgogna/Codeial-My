cd461862b6046d7057094be2ba4f1e823af118d1
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-09-24 19:02:35 +0530

    done

--- models/post.js
+++ models/post.js
@@ -17,7 +17,8 @@ const postSchema=new mongoose.Schema({
     }
    ]
 },{
-    timestamps:true
+    timestamps:true,
+    toJSON: {virtuals: true}
 });
 
 const Post=mongoose.model("Post",postSchema);
